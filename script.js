880eed758ebebf77ca68ad724c71fb2606eb8d04
Gokdeniz149 <gokler_520@hotmail.com>
2025-09-18 15:54:14 +0300

    Roman Numeral Converter

---
+++ script.js
@@ -0,0 +1,50 @@
+const button = document.getElementById("convert-btn");
+const number = document.getElementById("number");
+const output = document.getElementById("output");
+
+const romanNumerals = [
+    { value: 1000, numeral: "M" },
+    { value: 900, numeral: "CM" },
+    { value: 500, numeral: "D" },
+    { value: 400, numeral: "CD" },
+    { value: 100, numeral: "C" },
+    { value: 90, numeral: "XC" },
+    { value: 50, numeral: "L" },
+    { value: 40, numeral: "XL" },
+    { value: 10, numeral: "X" },
+    { value: 9, numeral: "IX" },
+    { value: 5, numeral: "V" },
+    { value: 4, numeral: "IV" },
+    { value: 1, numeral: "I" }
+];
+
+button.addEventListener("click", () => {
+    if (number.value === "") {
+        output.classList.remove("hidden", false);
+        output.classList.toggle("alert", true);
+        output.innerText = "Please enter a valid number";
+    }
+    else if (parseInt(number.value) <= -1) {
+        output.classList.remove("hidden", false);
+        output.classList.toggle("alert", true);
+        output.innerText = "Please enter a number greater than or equal to 1";
+    }
+    else if (parseInt(number.value) >= 4000) {
+        output.classList.remove("hidden", false);
+        output.classList.toggle("alert", true);
+        output.innerText = "Please enter a number less than or equal to 3999";
+    }
+    else {
+        output.classList.remove("hidden", false);
+        output.classList.toggle("alert", false);
+        let result = "";
+        for (const {value, numeral} of romanNumerals) {
+            while (number.value >= value) {
+                result += numeral;
+                number.value -= value;
+            }
+        }
+        output.innerText = result;
+    }
+    return;
+});
